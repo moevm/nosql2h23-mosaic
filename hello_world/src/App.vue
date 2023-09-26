952a4849af979f55147b34ba903c4b7c5aeb524d
xaninam <xanina.maria.02@mail.ru>
2023-09-27 00:43:01 +0300

    init done

---
+++ hello_world/src/App.vue
@@ -0,0 +1,35 @@
+<script>
+// import { driver, auth } from 'neo4j-driver'
+//
+// export const neo4jDriver = driver(
+//     process.env.NEO4J_URI ?? '',
+//     auth.basic(
+//         process.env.NEO4J_USERNAME ?? 'username',
+//         process.env.NEO4J_PASSWORD ?? 'password'
+//     )
+// );
+// export default {
+//   methods: {
+//     handleClick() {
+//       const session = neo4jDriver.session();
+//       try {
+//         session.run('MATCH (n) RETURN n LIMIT 1');
+//         return 'Connection successful'
+//       } catch (error) {
+//         throw new Error('Connection to neo4j failed')
+//       } finally {
+//         session.close();
+//       }
+//     }
+//   }
+// }
+</script>
+
+<template>
+<!--<div>-->
+<!--  <button class="test" @click="handleClick()"></button>-->
+<!--</div>-->
+</template>
+
+<style scoped>
+</style>
